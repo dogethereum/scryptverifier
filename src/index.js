359ec6823865054209a1e709d9895ac70a02fcdd
Ismael Bejarano <ismael.bejarano@coinfabrik.com>
2017-09-12 16:11:50 -0300

    Add script to test verifier & sample input

--- src/index.js
+++ src/index.js
@@ -1,14 +1,33 @@
 const Contract = require('truffle-contract');
 const Web3 = require('web3');
-const ScryptTestJson = require('../build/contracts/ScryptTest.json');
+const ScryptVerifierJson = require('../build/contracts/ScryptVerifier.json');
+
+const provider = new Web3.providers.HttpProvider('http://localhost:8545');
+const web3 = new Web3(provider);
+
+const ScryptVerifier = Contract(ScryptVerifierJson);
+ScryptVerifier.setProvider(provider);
+ScryptVerifier.defaults({
+  from: web3.eth.accounts[0],
+  gas: 4700000,
+});
+// ScryptTest.synchronization_timeout = 1000;
 
 async function main() {
   try {
-    const ScryptTest = Contract(ScryptTestJson);
-    ScryptTest.setProvider(new Web3.providers.HttpProvider('http://localhost:8545'));
-    const scryptTest = await ScryptTest.deployed();
-    const input = await scryptTest.input.call();
-    console.log(`Input: ${input.toString(16)}`);
+    const scryptVerifier = await ScryptVerifier.deployed();
+
+    const input = require('./run.json');
+
+    const blockHeader = web3.toAscii(`0x${input[0].input}`)
+    const blockHash = `0x${input[2049].output}`;
+
+    const result = await scryptVerifier.submit(blockHeader, blockHash, 0);
+    console.log(`Submit: ${result.tx}`);
+
+    const blockData = await scryptVerifier.blocks.call(blockHash);
+    console.log(`Result: ${blockData}`);
+
   } catch (err) {
     console.log(`Error: ${err} ${err.stack}`);
   }
