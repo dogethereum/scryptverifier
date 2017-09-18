470941f54a19d6e63072bc206878daed8813373a
Ismael Bejarano <ismael.bejarano@coinfabrik.com>
2017-09-18 18:48:28 -0300

    Add utility to create a json with intermediate result of applying scrypt
    For example
    
    > python main.py --output run.json 00000001cef715f6b8c64f3b898f1ef6081ddbae507650523b9e3a53ccbbb910279f633067a8ca9f52efe146c4b3edd3925982add09b500d9b5c60738399d5f1066b2a754ebb17b81d018ea7d4592d01
    
    Will apply scryp to the input data and will record the intermediate steps in run.json

---
+++ pyscrypt/main.py
@@ -0,0 +1,69 @@
+import argparse
+import json
+from binascii import hexlify, unhexlify
+from pyscrypt import hash
+from sha3 import keccak_256
+
+parser = argparse.ArgumentParser(description='Generate scrypt output')
+parser.add_argument('header', help='Block header in hexadecimal')
+parser.add_argument('--output', metavar='filename.json', help='Output file with proccessed header')
+
+def b32dec(data):
+    return b''.join([data[i:i+4][::-1] for i in range(0, len(data), 4)])
+
+def tobytes(data):
+    out = [ ]
+    for i in data:
+        out.append((i >> 0) & 0xff)
+        out.append((i >> 8) & 0xff)
+        out.append((i >> 16) & 0xff)
+        out.append((i >> 24) & 0xff)
+    return bytes(out)
+
+
+def sha3(data):
+    s = keccak_256()
+    s.update(data)
+    return s.digest()
+
+class ScryptOutput:
+    def __init__(self, filename):
+        self.filename = filename
+        self.rounds = []
+
+    def __call__(self, input, output, step):
+        if step == 2049:
+            output = b32dec(output[::-1])
+        elif step == 0:
+            pass
+        else:
+            input = tobytes(input)
+            output = tobytes(output)
+        round = {
+            "step": step,
+            "input": hexlify(b32dec(input)).decode('ascii'),
+            "input_hash": hexlify(sha3(b32dec(input))).decode('ascii'),
+            "output": hexlify(b32dec(output)).decode('ascii'),
+            "output_hash": hexlify(sha3(b32dec(output))).decode('ascii')
+        }
+        if self.filename:
+            self.rounds.append(round)
+        else:
+            print("{}".format(json.dumps(round, indent=2)))
+
+    def save(self):
+        if self.filename:
+            with open(self.filename, 'w') as f:
+                json.dump(self.rounds, f, indent=2)
+
+def main():
+    args = parser.parse_args()
+    header = unhexlify(args.header)
+    scryptOutput = ScryptOutput(args.output or None)
+    input = b32dec(header)
+    hash(input, input, 1024, 1, 1, 32, scryptOutput)
+    scryptOutput.save()
+
+
+if __name__ == '__main__':
+    main()
