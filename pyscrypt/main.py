e8502ed32c46d5ca638a1986b8f0142a22bc687b
Ismael Bejarano <ismaelbej@gmail.com>
2017-10-19 05:41:36 -0300

    Use input/output as is
    # Conflicts:
    #	pyscrypt/main.py

--- pyscrypt/main.py
+++ pyscrypt/main.py
@@ -8,11 +8,13 @@ parser = argparse.ArgumentParser(description='Generate scrypt output')
 parser.add_argument('header', help='Block header in hexadecimal')
 parser.add_argument('--output', metavar='filename.json', help='Output file with proccessed header')
 
+
 def b32dec(data):
     return b''.join([data[i:i+4][::-1] for i in range(0, len(data), 4)])
 
+
 def tobytes(data):
-    out = [ ]
+    out = []
     for i in data:
         out.append((i >> 0) & 0xff)
         out.append((i >> 8) & 0xff)
@@ -26,6 +28,7 @@ def sha3(data):
     s.update(data)
     return s.digest()
 
+
 class ScryptOutput:
     def __init__(self, filename):
         self.filename = filename
@@ -33,9 +36,9 @@ class ScryptOutput:
 
     def __call__(self, input, output, step, extra={}):
         if step == 2049:
-            output = b32dec(output[::-1])
+            output = b32dec(output)
         elif step == 0:
-            pass
+            input = b32dec(input)
         else:
             input = tobytes(input)
             output = tobytes(output)
@@ -48,9 +51,9 @@ class ScryptOutput:
         }
         if 'input2' in extra:
             input2 = tobytes(extra['input2'])
-            round['input2'] = hexlify(b32dec(input2)).decode('ascii');
-            round['input2_hash'] = hexlify(sha3(b32dec(input2))).decode('ascii');
-            round['input2_index'] = extra['input2_index'];
+            round['input2'] = hexlify(b32dec(input2)).decode('ascii')
+            round['input2_hash'] = hexlify(sha3(b32dec(input2))).decode('ascii')
+            round['input2_index'] = extra['input2_index']
         if self.filename:
             self.rounds.append(round)
         else:
@@ -61,10 +64,16 @@ class ScryptOutput:
             with open(self.filename, 'w') as f:
                 json.dump(self.rounds, f, indent=2)
 
+
 def main():
     args = parser.parse_args()
     header = unhexlify(args.header)
     scryptOutput = ScryptOutput(args.output or None)
+    input = header
+    output = hash(input, input, 1024, 1, 1, 32, scryptOutput)
+    if args.output:
+        print("Input: {}".format(hexlify(input)))
+        print("Output: {}".format(hexlify(output)))
     input = b32dec(header)
     hash(input, input, 1024, 1, 1, 32, scryptOutput)
     scryptOutput.save()
