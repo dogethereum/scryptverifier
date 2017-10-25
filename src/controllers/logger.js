ac19bb3ceafcb96bf75b7343d4935f0006da7cb9
Ismael Bejarano <ismael.bejarano@coinfabrik.com>
2017-10-25 14:39:08 -0300

    Use winston as backend logger

---
+++ src/controllers/logger.js
@@ -0,0 +1,19 @@
+const winston = require('winston');
+const config = require('../config');
+
+const logger = new (winston.Logger)();
+
+if (config.logFile) {
+  logger.add(winston.transports.File, {
+    level: 'debug',
+    timestamp: true,
+    filename: config.logFile,
+  });
+}
+
+logger.add(winston.transports.Console, {
+  level: 'info',
+  timestamp: true,
+});
+
+module.exports = logger;
