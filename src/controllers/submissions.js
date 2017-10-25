15ac7dd12d627885941898098882d0f8007a6e68
Ismael Bejarano <ismael.bejarano@coinfabrik.com>
2017-10-25 15:18:59 -0300

    Query single submission/refactor controller

--- src/controllers/submissions.js
+++ src/controllers/submissions.js
@@ -2,27 +2,43 @@ const ethereum = require('./ethereum');
 
 const NUM_SUBMISSIONS = 50;
 
-async function listSubmissions() {
-  const scryptVerifier = await ethereum.getScryptVerifier();
-  const numSubmissions = await scryptVerifier.getNumSubmissions()
-    .then(result => parseInt(result, 10));
+class SubmissionsController {
+  constructor() {
+    this.init();
+  }
 
-  const start = numSubmissions > NUM_SUBMISSIONS ? numSubmissions - NUM_SUBMISSIONS : 0;
-  const count = numSubmissions > NUM_SUBMISSIONS ? NUM_SUBMISSIONS : numSubmissions;
-  const hashes = await scryptVerifier.getSubmissionsHashes(start, count);
+  async init() {
+    this.scryptVerifier = await ethereum.getScryptVerifier();
+  }
 
-  const submissions = await Promise.all(
-    hashes.map(hash => scryptVerifier.getSubmission(hash).then(result => ({
+  getSubmissionInternal(hash) {
+    return this.scryptVerifier.getSubmission(hash).then(result => ({
       hash: result[0],
       input: result[1],
       submitter: result[2],
       timestamp: result[3],
-    }))),
-  );
+    }));
+  }
 
-  return { submissions };
+  async getSubmission(hash) {
+    const submission = await this.getSubmissionInternal(hash);
+    return { submission };
+  }
+
+  async listSubmissions() {
+    const numSubmissions = await this.scryptVerifier.getNumSubmissions()
+      .then(result => parseInt(result, 10));
+
+    const start = numSubmissions > NUM_SUBMISSIONS ? numSubmissions - NUM_SUBMISSIONS : 0;
+    const count = numSubmissions > NUM_SUBMISSIONS ? NUM_SUBMISSIONS : numSubmissions;
+    const hashes = await this.scryptVerifier.getSubmissionsHashes(start, count);
+
+    const submissions = await Promise.all(
+      hashes.map(hash => this.getSubmissionInternal(hash)),
+    );
+
+    return { submissions };
+  }
 }
 
-module.exports = {
-  listSubmissions,
-};
+module.exports = new SubmissionsController();
--- src/routes/submissions.js
+++ src/routes/submissions.js
@@ -3,11 +3,26 @@ const Submissions = require('../controllers/submissions');
 
 const router = express.Router();
 
-router.get('/', async (re, res) => {
+router.get('/', async (req, res) => {
   try {
     const { submissions } = await Submissions.listSubmissions();
     res.json({
       submissions,
+    });
+  } catch (ex) {
+    res.status(ex.status || 500);
+    res.json({
+      errors: [ex.message],
+    });
+  }
+});
+
+router.get('/:hash', async (req, res) => {
+  try {
+    const { hash } = req.params;
+    const { submission } = await Submissions.getSubmission(hash);
+    res.json({
+      submission,
     });
   } catch (ex) {
     res.status(ex.status || 500);
