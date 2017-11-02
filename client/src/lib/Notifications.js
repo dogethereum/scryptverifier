8ec76040940d8fe9f0472e8064060c5803950713
Ismael Bejarano <ismael.bejarano@coinfabrik.com>
2017-11-02 16:44:53 -0300

    Try to reconnect when disconnected

--- client/src/lib/Notifications.js
+++ client/src/lib/Notifications.js
@@ -3,7 +3,14 @@ import io from 'socket.io-client';
 import config from '../config';
 
 class NotificationController extends EventEmitter {
+  constructor() {
+    super();
+    this.reconnectEvents = this.reconnectEvents.bind(this);
+    this.subscribed = false;
+  }
+
   subscribe() {
+    this.subscribed = true;
     this.socket = io(`${config.host}:${config.port}`);
     this.socket.on('connect', () => {
       this.socket.emit('register');
@@ -25,11 +32,23 @@ class NotificationController extends EventEmitter {
     });
     this.socket.on('disconnect', () => {
       this.socket.emit('unregister');
+      this.socket.close();
+      this.socket = null;
+      if (this.subscribed) {
+        setTimeout(this.reconnectEvents, 60 * 1000);
+      }
     });
+  }
+
+  reconnectEvents() {
+    if (!this.socket) {
+      this.subscribe();
+    }
   }
 
   unsubscribe() {
     if (this.socket) {
+      this.subscribed = false;
       this.socket.emit('unregister');
       this.socket.close();
       this.socket = null;
